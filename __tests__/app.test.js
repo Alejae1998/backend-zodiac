ecebe65f90d9a2e4f531e0779a5b7b32ff978cb0
Alejae1998 <alejagr1998@gmail.com>
2022-07-25 15:07:17 -0700

    test 1 and 2 pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,29 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { zodiacs } = require('../data/zodiacs');
 
-describe('backend-express-template routes', () => {
+describe('backend-zodiac routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('zodiac should return a list of zodiacs', async () => {
+    const res = await request(app).get('/zodiacs');
+    const expected = zodiacs.map((zodiac) => {
+      return { id: zodiac.id, name: zodiac.name };
+    });
+    expect(res.body).toEqual(expected);
+  });
+
+  it('/zodiacs/:id should return zodiac detail', async () => {
+    const res = await request(app).get('/zodiacs/1');
+    const aquarius = {
+      id: '1',
+      name: 'aquarius',
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer',
+    };
+    expect(res.body).toEqual(aquarius);
   });
   afterAll(() => {
     pool.end();
